05cfa57b8f03737980a48152181d6f96b8656445
Crasneanu Cristian <crasneanu.cristian@gmail.com>
2019-09-12 16:17:31 +0300

    [ Tasks ] Make methods public

--- Sources/Tasks/Configurator/TaskConfigurator.swift
+++ Sources/Tasks/Configurator/TaskConfigurator.swift
@@ -15,31 +15,31 @@ public class TaskConfigurator {
     
     private init() { }
     
-    func photoCommentsTask() -> PhotoCommentsTaskProtocol {
+    public func photoCommentsTask() -> PhotoCommentsTaskProtocol {
         return self.shouldRunLocally ? PhotoCommentsLocalTask() : PhotoCommentsNetworkTask()
     }
     
-    func photoTask() -> PhotoTaskProtocol {
+    public func photoTask() -> PhotoTaskProtocol {
         return self.shouldRunLocally ? PhotoLocalTask() : PhotoNetworkTask()
     }
     
-    func imageTask() -> ImageTaskProtocol {
+    public func imageTask() -> ImageTaskProtocol {
         return self.shouldRunLocally ?  ImageLocalTask() : ImageNetworkTask()
     }
     
-    func photoCollectionTask() -> PhotoCollectionTaskProtocol {
+    public func photoCollectionTask() -> PhotoCollectionTaskProtocol {
         return self.shouldRunLocally ?  PhotoCollectionLocalTask() : PhotoCollectionNetworkTask()
     }
     
-    func locationAddressTask() -> LocationAddressTaskProtocol {
+    public func locationAddressTask() -> LocationAddressTaskProtocol {
         return self.shouldRunLocally ?  LocationAddressLocalTask() : LocationAddressNetworkTask()
     }
     
-    func locationEntitiesTask() -> LocationEntitiesTaskProtocol {
+    public func locationEntitiesTask() -> LocationEntitiesTaskProtocol {
         return self.shouldRunLocally ?  LocationEntitiesLocalTask() : LocationEntitiesNetworkTask()
     }
     
-    func photosTask() -> PhotosTaskProtocol {
+    public func photosTask() -> PhotosTaskProtocol {
         return self.shouldRunLocally ? PhotosLocalTask() : PhotosNetworkTask()
     }
 }
